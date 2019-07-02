e94500fe43372665b10881d9cf48878567a866b2
Steshoff <52158835+Steshoff@users.noreply.github.com>
2019-07-02 14:50:28 +0300

    Add sound, third commit

--- em_detect.py
+++ em_detect.py
@@ -4,6 +4,9 @@ import pickle
 import numpy as np
 from time import sleep
 from PIL import ImageFont, ImageDraw, Image
+detector = dlib.get_frontal_face_detector() #Face detector
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat") #Landmark identifier. Set the filename to whatever you named
+facerec = dlib.face_recognition_model_v1('dlib_face_recognition_resnet_model_v1.dat')
 def get_landmarks(image):
     detections = detector(image, 1)
     for k,d in enumerate(detections): #For all detected face instances individually
@@ -35,44 +38,47 @@ def get_landmarks(image):
         face_descriptor=0
         #xlist,ylist,meannp,
     return landmarks_vectorised,detections, face_descriptor
-emo=['Радость', 'Удивление', 'Грусть', 'Злость', 'Отвращение', 'Презрение', 'Страх']
-font= ImageFont.truetype("DejaVuSans.ttf", 32)
-f=open('svm_dat.dat','rb')
-clf=pickle.load(f)
-f.close
-video_capture = cv2.VideoCapture(0) #Webcam object
-video_capture.set(3, 380)
-video_capture.set(4, 240)
-detector = dlib.get_frontal_face_detector() #Face detector
-predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat") #Landmark identifier. Set the filename to whatever you named
-facerec = dlib.face_recognition_model_v1('dlib_face_recognition_resnet_model_v1.dat')
-emo_sm=0
-alfa=0.9
-while True:
-    ret, frame = video_capture.read()
-    ''' gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
-    clahe_image = clahe.apply(gray) '''
-    l_mv, det,f_n = get_landmarks(frame)
-    #l_mv, det = get_landmarks(clahe_image)
-    if l_mv!=0:
-        dat=[]
-        dat.append(f_n)
-        #dat.append(l_mv) 
-        em_n=clf.predict(dat)
-        emo_sm=alfa*(em_n-1)+(1-alfa)*emo_sm
-        print(em_n,emo[round(float(em_n-1))],emo_sm+1)
-        cv2.rectangle(frame, (det[0].left(), det[0].top()), (det[0].right(), det[0].bottom()), (0, 0, 255), 2)
-        #cv2.putText(frame, emo[round(float(emo_sm))], (det[0].left(), det[0].top()), font, 2, (0, 255, ), 2)
-
-        img_pil = Image.fromarray(frame)
-        draw = ImageDraw.Draw(img_pil)
-        draw.text( (det[0].left(), det[0].top()-40),  emo[round(float(emo_sm))], font=font, fill=(0,255,0))
-        frame = np.array(img_pil)
-
-    cv2.imshow('camera', frame) #Display the frame
-    #sleep(0.3)
-    if cv2.waitKey(1) & 0xFF == ord('q'): #Exit program when the user presses 'q'
-        break
-video_capture.release()
-cv2.destroyAllWindows()
+def em_7(em_ind):
+    nn=10
+    emo=['Радость', 'Удивление', 'Грусть', 'Злость', 'Отвращение', 'Презрение', 'Страх']
+    font= ImageFont.truetype("DejaVuSans.ttf", 32)
+    f=open('svm_dat.dat','rb')
+    clf=pickle.load(f)
+    f.close
+    video_capture = cv2.VideoCapture(0) #Webcam object
+    video_capture.set(3, 380)
+    video_capture.set(4, 240)
+    emo_sm=0
+    alfa=0.9
+    en_count=0
+    #for i in range(1,nn):
+    for i in range(1,nn):
+        ret, frame = video_capture.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+        clahe_image = clahe.apply(gray)
+        l_mv, det,f_n = get_landmarks(frame)
+        #l_mv, det = get_landmarks(clahe_image)
+        if l_mv!=0:
+            dat=[]
+            dat.append(f_n)
+            #dat.append(l_mv) 
+            em_n=clf.predict(dat)
+            emo_sm=alfa*(em_n-1)+(1-alfa)*emo_sm
+            ##print(em_n,emo[round(float(em_n-1))],emo_sm+1)
+            cv2.rectangle(frame, (det[0].left(), det[0].top()), (det[0].right(), det[0].bottom()), (0, 0, 255), 2)
+            #cv2.putText(frame, emo[round(float(emo_sm))], (det[0].left(), det[0].top()), font, 2, (0, 255, ), 2)
+            img_pil = Image.fromarray(frame)
+            draw = ImageDraw.Draw(img_pil)
+            draw.text( (det[0].left(), det[0].top()-40),  emo[round(float(emo_sm))], font=font, fill=(0,255,0))
+            frame = np.array(img_pil)
+            if em_n==em_ind:
+                en_count+=1
+        cv2.waitKey(1)
+        cv2.imshow('camera', frame) #Display the frame
+        #sleep(0.3)
+    en_count=en_count/nn
+    if em_ind==7:  
+        video_capture.release()
+        cv2.destroyAllWindows()  
+    return en_count
---
+++ test_pr.py
@@ -0,0 +1,26 @@
+from time import sleep
+from pygame import mixer
+from em_detect import em_7
+mixer.init()
+emo=['Радость', 'Удивление', 'Грусть', 'Злость', 'Отвращение', 'Презрение', 'Страх']
+sound=['1.mp3', '2.mp3', '3.mp3', '4.mp3', '5.mp3', '6.mp3', '7.mp3']
+for i, em in enumerate(emo):
+    em_res=0
+    while em_res<0.7:
+        mixer.music.load(sound[i])
+        mixer.music.play()
+        print("Покажите нам "+ em)
+        em_res=em_7(i+1)
+        if em_res>=0.7:
+            mixer.music.load('8.mp3')
+            mixer.music.play()
+            print("Молодец")
+            sleep(2)
+        else:
+            mixer.music.load('9.mp3')
+            mixer.music.play()
+            print("Попробуй еще")
+            sleep (2)
+            
+            
+       
